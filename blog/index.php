c9d0f7fd3adbe8d0d7f3860ae58734462c06b729
Richard Rutter <rich@clagAir-2.local>
2013-04-24 16:11:24 +0100

    added more example pages and some extra styles

--- blog/index.php
+++ blog/index.php
@@ -1,7 +1,7 @@
 <?php
 $dr = str_replace($_SERVER['SCRIPT_NAME'], '/includes/', $_SERVER['SCRIPT_FILENAME']);
 $title = "Seventy Penguins";
-$title = "Elements of Typographic Style Applied to the Web";
+$title = "Typographic Style Applied to the Web";
 include($dr . "head.inc.php");
 ?>
 
@@ -12,29 +12,74 @@ include($dr . "head.inc.php");
 
 <article>
 <p class="published"><abbr title="2005-06-07T02:22:59+01:00">7 June 2005</abbr></p>
-<h1><?php echo $title ?></h1>
 
-<div class="meta">
-	<p class="published"><abbr title="2005-06-07T02:22:59+01:00">7 June 2005</abbr></p>
-	<p class="categories">§  <a href="/archive/typography/" title="View all posts relating to Typography.">Typography</a> </p>
-</div>
+<h1><?php echo $title ?></h1>
 
 <section>
 
-<?php /*
+<figure class="inline"><a href="http://www.flickr.com/photos/clagnut/356030308/"><img src="/i/bestsongs2006.jpg" alt="Artwork of The Best Songs I Bought In 2006 Ever, featuring the Gone-but-not-forgotten Hanbury Ballroom"></a></figure>
+
+	<p>A pleasing number of people have been clamouring for this: the track
+ listing to my annual compilation, ‘The Best Songs I Bought In 2006 
+Ever’. It was another good year – remember this is compiled from music I
+ bought this year, but was not necessarily released this year.</p>
+
+	<h3>The Best Songs I Bought In 2006 Ever</h3>
+
+	<ol>
+	<li><cite>Roscoe</cite> by Midlake from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000F3ALAG/jalfrezi-21/">Trials of Van Occupanther</a></li>
+<li><cite>This Month, Day 10</cite> by <abbr><abbr>CSS</abbr></abbr> from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000MCHEEG/jalfrezi-21/">Cansei De Ser Sexy</a></li>
+<li><cite>Home Altars Of Mexiko</cite> by Art Brut from the single <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000AZ6NI8/jalfrezi-21/">Good Weekend</a></li>
+<li><cite>Dirty Barry Stole The Bluebird</cite> by The Fratellis from the single <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000H80KM8/jalfrezi-21/">Chelsea Dagger</a></li>
+<li><cite>Pile of Gold</cite> by The Blow from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000I2IYJE/jalfrezi-21/">Paper Television</a></li>
+<li><cite>Karma Police</cite> by Easy Star All*stars from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000GUJZ5K/jalfrezi-21/">Radiodread</a></li>
+<li><cite>Cold &amp; Wet</cite> by Bonnie ‘Prince’ Billy from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000FS9LM0/jalfrezi-21/">The Letting Go</a></li>
+<li><cite>Elevate</cite> by The People’s Revolutionary Choir from the single <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000BCHJH4/jalfrezi-21/">Elevate</a></li>
+<li><cite>Such Great Heights</cite> by Iron &amp; Wine from the single <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000GG4MXE/jalfrezi-21/">Such Great Heights</a></li>
+<li><cite>Pull The Curtains</cite> by Grandaddy from the <abbr><abbr>EP</abbr></abbr> <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000ARHNKI/jalfrezi-21/">Excerpts from the Diary of Todd Zilla</a></li>
+<li><cite>Woman</cite> by Wolfmother from the single <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000G03RQ8/jalfrezi-21/">Woman</a></li>
+<li><cite>Upon This Tidal Wave Of Young Blood</cite> by Clap Your Hands Say Yeah from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000C5RSAS/jalfrezi-21/">Clap Your Hands Say Yeah</a></li>
+<li><cite>Mount Wroclai (Idle Days)</cite> by Beirut from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000IJ7MCC/jalfrezi-21/">Gulag Orkestar</a></li>
+<li><cite>Young Folks</cite> by Peter Bjorn &amp; John from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000FA58IE/jalfrezi-21/">Writer’s Block</a></li>
+<li><cite>Cheated Hearts</cite> by Yeah Yeah Yeahs from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000EHSK24/jalfrezi-21/">Show Your Bones</a></li>
+<li><cite>Rent a Wreck</cite> by Suburban Kids with Biblical Names from the <abbr><abbr>EP</abbr></abbr> <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B0002W18PM/jalfrezi-21/">#1</a></li>
+<li><cite>Only Waiting</cite> by The Aliens from the <abbr><abbr>EP</abbr></abbr> <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000F5GI0Q/jalfrezi-21/">Alienoid Starmonica</a></li>
+<li><cite>Ballad Of Bitter Honey</cite> by Eef Barzelay from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000E6G7JI/jalfrezi-21/">Bitter Honey</a></li>
+<li><cite>Black Swan</cite> by Thom Yorke from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000FPYNR6/jalfrezi-21/">The Eraser</a></li>
+<li><cite>Assassin</cite> by Muse from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000G09OEC/jalfrezi-21/">Black Holes &amp; Revelations</a></li>
+<li><cite>(Wading Through) The Waters Of My Time</cite> by Richard Hawley from the album <a href="http://www.amazon.co.uk/exec/obidos/ASIN/B000AMSJQK/jalfrezi-21/">Coles Corner</a></li>
+</ol>
+	<p>These tracks just about squeeze on to a <abbr><abbr>CD</abbr></abbr>,
+ so if you’d like a copy, put together your own compilation of the year 
+and send it to me – you’ll get a copy of the above in return. <a href="http://clagnut.com/contact/">Email me</a> for details. <strong>Update</strong>: I stuck these in <a href="http://clkuk.tradedoubler.com/click?p=23708&amp;a=1330812&amp;url=http://phobos.apple.com/WebObjects/MZStore.woa/wa/viewIMix?id=212661948">an iMix</a> for your iTunes delectation.</p>	
+
+
+
 <p>I’ve just come back from a week with Her Indoors in the West Country; a few days in Devon and a few more in Cornwall (<a href="http://www.flickr.com/photos/clagnut/sets/421788/" title="my Flickr photoset">there are photos</a> – it’s a beautiful part of the world). While there I bought five books, all of which were <a href="http://www.penguin.co.uk/">Penguin</a> paperbacks.</p>
-
-
-
-<p>Penguin are currently celebrating their <a href="http://www.happybirthdaypenguin.com/" title="Happy Birthday Penguin minisite">seventieth birthday</a> by publishing a special series of seventy short paperbacks priced at a measly £1.50 each. The <a href="http://www.happybirthdaypenguin.com/" title="Happy Birthday Penguin minisite">spectrum of authors</a> is astonishing, ranging from contemporary novelists such as Zadie Smith and Hunter S. Thompson, through non-fiction like Eric Schlosser and Simon Schama and classic authors including Roald Dahl, George Orwell and <abbr>PG</abbr> Wodehouse, to extracts from key publications in Penguin’s history, such as Homer’s Odyssey and the trial over Lady Chatterley’s Lover. To top it, each book boasts a bespoke cover – the <a href="http://www.penguin.co.uk/nf/Book/BookDisplay/0,,0_0140912711,00.html">box set</a> was extremely tempting. In the end though I rather randomly bought four of the series: <a href="http://www.penguin.co.uk/nf/Book/BookDisplay/0,,0_0141022515,00.html" title="Penguin: Otherwise Pandemonium">Nick Hornby</a>, <a href="http://www.penguin.co.uk/nf/Book/BookDisplay/0,,0_0141022744,00.html" title="Penguin: Design Faults in the Volvo 760 Turbo">Will Self</a>, <a href="http://www.penguin.co.uk/nf/Book/BookDisplay/0,,0_0141022302,00.html" title="Penguin: The Great Wall of China">Franz Kafka</a> and <a href="http://www.penguin.co.uk/nf/Book/BookDisplay/0,,0_0141022175,00.html" title="Penguin: My Side of the Matter">Truman Capote</a>.</p>
-
-*/ ?>
 
 <p>The fifth book I bought was <a href="http://www.amazon.co.uk/exec/obidos/ASIN/0713998393/jalfrezi-21/" title="More info on Amazon">Penguin by Design – a Cover Story</a> by Phil Baines. Penguin books have a rather illustrious heritage, with legendary typographer <a href="http://en.wikipedia.org/wiki/Jan_Tschichold" title="Wikipedia - Jan Tschichold">Jan Tschichold</a> responsible for the iconic orange spines, three-pane covers with <a href="http://www.identifont.com/show?MB" title="Identifont">Gill Sans</a>, and of course serious attention to detail in the typesetting.</p>
 
 <h2>This is a heading level 2</h2>
 
-<p>Phil Baines takes us through the seventy years of history in the design of Penguin paperback covers, a constantly evolving part of Britain’s culture and design history. The book is lavishly illustrated, with many full size covers, and goes into detail of individual titles. Fascinating too is the coverage of changing role of artists and designers throughout Penguin’s history. It’s a luscious book for anyone interested in typographical-related design. </p>
+<p>Phil Baines takes us through the seventy years of history in the design of Penguin paperback covers, a constantly evolving part of Britain’s culture and design history. </p>
+
+<figure class='inline'><img src="/i/hack.jpg" alt="Cover of Penguin by Design" /><figcaption>The Hack Farm team all enjoying themselves doing something or other.</figcaption></figure>
+
+<p>Phil Baines takes us through the seventy years of history in the design of Penguin paperback covers, a constantly evolving part of Britain’s culture and design history. <ins>The book</ins> is <del>lavishly</del> illustrated, with many full size covers, and goes into detail of individual titles. Fascinating too is the coverage of changing role of artists and designers throughout Penguin’s history.</p>
+
+
+<ul>
+	<li>allowing users to skip the lookup altogether (maybe the user doesn’t know their postcode)</li>
+	<li>helpfully dealing with a failed lookup (edit the postcode or skip 
+the lookup – often databases are not kept up to date and so valid 
+postcodes are not recognised)</li>
+</ul>
+<ol>
+	<li>allow users to type a different postcode (their work address instead of their home address)</li>
+	<li>deal with addresses not present in the list returned</li>
+</ol>
+
+<p>It’s a luscious book for anyone interested in typographical-related design. </p>
 
 <h3>Heading level 3</h3>
 
@@ -44,13 +89,19 @@ include($dr . "head.inc.php");
 
 <section>
 
-<h4>Heading level 4</h4>
 
-<p>As an aside, a few days before I bought the books in Cornwall, Her Indoors and I visited <a href="http://www.nationaltrust.org.uk/main/w-vh/w-visits/w-findaplace/w-greenway.htm" title="The National Trust">Greenway</a>, a former home of Agatha Christie. Only afterwards did I discover that, in 1935, Allen Laine – the creator of Penguin books – visited Agatha Christie at this same cottage and had his vision of the cheap quality paperback. Christie was one of the first authors to be published by Penguin.</p>
+<h4>Heading level 4</h4>
+<p>As an aside, a few days before I bought the books in Cornwall, Her Indoors and I visited <a href="http://www.nationaltrust.org.uk/main/w-vh/w-visits/w-findaplace/w-greenway.htm" title="The National Trust">Greenway</a>, a former home of Agatha Christie.</p>
+
+
+<p>Only afterwards did I discover that, in 1935, Allen Laine – the creator of Penguin books – visited Agatha Christie at this same cottage and had his vision of the cheap quality paperback. Christie was one of the first authors to be published by Penguin.</p>
 </section>
 
 
 <footer>
+<div class="meta">
+	<p class="categories">§  <a href="/archive/typography/" title="View all posts relating to Typography.">Typography</a> </p>
+</div>
 
 </footer>
 
---
+++ store/Top Tunes 2006 | Clagnut § Music_files/global.js
@@ -0,0 +1,61 @@
+﻿$(document).ready(
+function() {
+	// hide stuff
+	$('#machine_tags').hide();
+
+	// deal with form controls with 'valueprompt'
+	// takes title and makes it the value unless there's already a value set
+	$('.valueprompt').attr("value", function() { return this.title });
+	$('.valueprompt').focus(
+		function() {
+			if(this.value == this.title) {
+				this.value = "";
+			}
+		}
+	);
+	$('.valueprompt').blur(
+		function() {
+			if(this.value == "") {
+				this.value = this.title;
+			}
+		}
+	);
+	
+	// toggle machine tags
+	$('#machine_tags_toggle').mouseover(
+		function() {
+			$('.arrow').addClass('hover');
+		}
+	);
+	$('#machine_tags_toggle').mouseout(
+		function() {
+			$('.arrow').removeClass('hover');
+		}
+	);
+	$('#machine_tags_toggle').click(
+		function() {
+			if($('#machine_tags_toggle .arrow').text() == "▼") {
+				$('#machine_tags_toggle .arrow').text('►')
+			} else {
+				$('#machine_tags_toggle .arrow').text('▼')
+			}
+			$('#machine_tags').toggle('fast');
+			return false;
+		}
+	);
+	
+	// swap out more images on hover
+	$('img.more').mouseover(
+		function() {
+			this.src = "/images/more-hover.png";
+		}
+	);
+	$('img.more').mouseout(
+		function() {
+			this.src = "/images/more.png";
+		}
+	);
+	
+});
+
+
