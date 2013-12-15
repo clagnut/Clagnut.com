b2f097c328ca1c186f9a06a02f3e7a3d85eb7f1b
Richard Rutter <rich@clagAir-2.local>
2013-12-15 17:04:04 +0000

    styling gallery

--- blog/index.php
+++ blog/index.php
@@ -19,8 +19,10 @@ include($dr . "header.inc.php");
 
 <h1><?php echo $title ?></h1>
 
+<div class="meta">
 <p class="published"><abbr title="2005-06-07T02:22:59+01:00">7th June 2005</abbr></p>
-
+<p class="categories">§ <a href="/archive/typography/" title="View all posts relating to Typography.">Typography</a></p>
+</div>
 </header>
 
 <section>
@@ -32,6 +34,48 @@ include($dr . "header.inc.php");
 <p>A pleasing number of people have been clamouring for this: the track listing to my annual compilation, ‘The Best Songs I Bought In 2006 Ever’. It was another good year – remember this is compiled from music I bought this year, but was not necessarily released this year.</p>
 
 </section>
+
+<div class="gallery">
+	<div class="gallery-photos">
+		<figure class="photo"><a href="http://flickr.com/photos/clagnut/17802430/"><img src="/i/flickr/1.jpg" alt="Crabbing"></a><figcaption>Crabbing</figcaption></figure>
+		<figure class="photo"><a href="http://flickr.com/photos/clagnut/17802565/"><img src="/i/flickr/3.jpg" alt="Passenger ferry"></a><figcaption>Passenger ferry pulling out</figcaption></figure>
+		<figure class="photo"><a href="http://flickr.com/photos/clagnut/17801254/"><img src="/i/flickr/4.jpg" alt="Herring gull"></a><figcaption>Herring gull</figcaption></figure>
+		<figure class="photo"><a href="http://flickr.com/photos/clagnut/17800540/"><img src="/i/flickr/5.jpg" alt="70 Pocket Penguins"></a><figcaption>70 Pocket Penguins</figcaption></figure>
+		<figure class="photo"><a href="http://flickr.com/photos/clagnut/17800512/"><img src="/i/flickr/6.jpg" alt="Fore Street, St Ives"></a><figcaption>Fore Street, St Ives</figcaption></figure>
+		<figure class="photo"><a href="http://flickr.com/photos/clagnut/17802456/"><img src="/i/flickr/2.jpg" alt="River Dart, Dittisham"></a><figcaption>River Dart, Dittisham</figcaption></figure>
+		<figure class="photo"><a href="http://flickr.com/photos/clagnut/17800962/"><img src="/i/flickr/7.jpg" alt="St Mawes"></a><figcaption>St Mawes</figcaption></figure>
+		<figure class="photo"><a href="http://flickr.com/photos/clagnut/17800279/"><img src="/i/flickr/8.jpg" alt="Tate St Ives"></a><figcaption>Tate St Ives</figcaption></figure>
+		<figure class="photo"><a href="http://flickr.com/photos/clagnut/17800983/"><img src="/i/flickr/9.jpg" alt="Fal estuary under thunderous skies"></a><figcaption>Fal estuary</figcaption></figure>
+	</div>
+</div>
+
+
+<aside class="data">
+	<div class="tags">
+		<h3>#</h3>
+		<ul>
+		<li><a href="http://clagnut.com/search/?q=Typography" rel="tag">Typography</a></li>
+		<li><a href="http://clagnut.com/search/?q=penguin" rel="tag">penguin</a></li>
+		<li><a href="http://clagnut.com/search/?q=book+design" rel="tag">book design</a></li>
+		<li><a href="http://clagnut.com/search/?q=cornwall" rel="tag">cornwall</a></li>
+		<li><a href="http://clagnut.com/search/?q=devon" rel="tag">devon</a></li>
+		<li><a href="http://clagnut.com/search/?q=isbntagged" rel="tag">isbntagged</a></li>
+		<li><a href="http://clagnut.com/search/?q=geotagged" rel="tag">geotagged</a></li>
+		<li><a href="http://clagnut.com/search/?q=Agatha+Christie" rel="tag">Agatha Christie</a></li>
+		<li><a href="http://clagnut.com/search/?q=Phil+Baines" rel="tag">Phil Baines</a></li>
+		</ul>	
+			
+		<div id="machine_tags">
+			<h4><a href="#machine_tags" class="arrow">►</a> Machine tags</h4>
+			
+			<ul>
+			<li><a href="http://clagnut.com/search?q=clagnut%3Apost%3D1484" rel="tag">clagnut:post=1484</a></li>
+			<li><a href="http://clagnut.com/search?q=geo%3Alat%3D50.382" rel="tag">geo:lat=50.382</a></li>
+			<li><a href="http://clagnut.com/search?q=geo%3Alon%3D-3.5879" rel="tag">geo:lon=-3.5879</a></li>
+			</ul>
+		</div>
+	</div>
+</aside>
 
 </article>
 
