516c94cd4da6b9644b1497d3eb99a1cd72652f4c
Richard Rutter <rich@clearleft.com>
2014-01-06 14:35:30 +0000

    added footer

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,3 +1,4 @@
 <footer class="global">
-<p>&copy; 2002&ndash;2014 Richard Rutter</p>
+<p class="copyright">&copy;&thinsp;2002&ndash;2014 Richard Rutter</p>
+<p><a href="http://twitter.com/clagnut" class="twitter icon">Follow me on Twitter</a> <a href="http://feeds.feedburner.com/Clagnut" class="RSS icon">RSS Feed</a>
 </footer>
