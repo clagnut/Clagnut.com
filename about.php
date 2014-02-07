30b9e2fd1d6e16bb3899d29ee48ef68490d50c08
Richard Rutter <rich@clagAir-2.local>
2014-02-07 10:06:19 +0000

    added more guff to about page

--- about.php
+++ about.php
@@ -44,6 +44,67 @@ include_once($dr . "writing_header.inc.php");
 </ul>
 
 
+
+So who are you and what’s this?
+
+    Richard Rutter. I’m a web site producer living in Brighton. I am a Director of Clearleft, a web design and accessibility consultancy also based in Brighton. Just so we’re clear, nothing I say say on this site necessarily represents the views of Clearleft; it’s all my personal opinion.
+
+    So this is a personal site consisting of a weblog (blog for short) and various other bits and pieces. A blog is a bit like a diary† in that it’s regularly updated and chronologically ordered. A key difference is that blogs tend to be littered with links to other web sites, often with commentary on said links.
+
+    Blogs usually have a theme running through them. This one has my personal perspective on Web development & design, visiting topics such as accessibility, usability and information architecture along the way. Don’t expect me to tediously stick to the theme though, I have got other things on my mind.
+
+    I encourage reader feedback so if I’m talking cobblers, if you’ve got an alternative viewpoint or simply concur with everything I say then please post a comment. Thank you.
+
+    † Some parts of the world are probably more familiar with the word journal.
+What’s a web site producer, then?
+
+    The term producer has crossed over from film and television. A film producer is responsible for the budget, management and general artistic or thematic direction. A web site producer looks after similar aspects of a web site project, but also takes on the director’s role. A film director determines what happens plot-wise in each scene and how the scene is lit, decorated and filmed within the context of the whole movie.
+
+    Thus a web site producer defines what happens on each screen: what users see, feel and interact with, and how that relates to other screens on the site. As with a film scene, what the web screen actually looks like is ultimately down to visual designers (not that we work in isolation, you understand).
+
+    A web site producer needs to combine project management, information architecture & usability with sound technical and aesthetic understanding. Blowing my own trumpet? Nah, I just want a raise.
+How did you get to be a web producer?
+
+    It all started with a degree in Chemical Engineering at the University of Bath. The degree led to Woodhill Engineering Consultants and five years of oil & gas safety engineering. Safety engineering involved huge spreadsheets estimating fatality rates on North Sea gas platforms and Alaskan oil fields. Morbid, but it kept you focused on the task at hand, and was enlivened by the occasional trip to an oil rig (I’ll regale you with the experience some time).
+
+    Always keen on a bit of one-upmanship, Woodhill agreed with my suggestion in 1994 that it would look good for them to have a web site. And so the seed was sown. Even now I remember fondly, the excitement back then as Netscape 2 catapulted into public beta testing. Suddenly you could change colours, fonts and backgrounds!
+
+    Come 2000, Great Yarmouth in February ganged up with the tedium that is quantified risk assessments and dealt me a good kicking. Through the red mist appeared James and Chris, two fine folks I’d befriended at Bath, who guided me into the open arms of Citria, a now-sadly-defunct web consultancy. The oil industry was kicked into touch; numeeja party time was the daddy now. I had become a web site producer. You did ask.
+So you designed this site then?
+
+    Yeah. Designed and built, although I’d never deign to call myself a designer in the artistic graphic design sense. I’m colour blind† for a start (you may have noticed), however colour, layout and typography can all be reasonably achieved by sticking to well-defined rules which do the job on a web site such as this.
+
+    Anyhow, since you’re about to ask, the clagnut front end is build in straightforward, semantic XHTML, i.e. where there’s a heading, I’ve used a heading tag. The layout and presentation uses fairly advanced CSS 2, but this means you’ll find almost all the HTML you need in HTML 2.0, aside from the odd <div> and <span>. It looks a bit crap in, um, crappy browsers, but of course the site is still navigable and readable.
+
+    I coded a bespoke CMS in the lovely PHP/MySQL combo. The CMS handles the weblog, searching, music database, photos and newsletter; well everything really.
+
+    [I guess I could have used Movable Type or some such tool but it kind of felt like cheating. And anyway, thanks to the demise of Citria, I had a bit of time on my hands which I wanted to fill relatively usefully.]
+
+    † Colour deficient strictly speaking. Protanomaly medically speaking. If you could turn the red down on your vision, you’d see how I see.
+Why does your text look mangled?
+
+    The main body text on clagnut is set in Lucida Bright. Most people with Lucida installed can see it fine, however it seems there is a very poor version of the typeface out there. This ancient version has little or no hinting for screen (it prints well though) and results in highly pixelated rendering. Sure I could set my default to Georgia (as I have with this paragraph) but I won't give up that easily. I've now provided an alternative style sheet method to offer visitors a choice - just flick the style switcher to something other than Lucida.
+Can anyone use your site’s style sheets, JavaScript, etc?
+
+    Yes, they are free for the taking (since you’re going to anyhow) although the site design isn’t. In return, a credit in the code and maybe a mention on your site would be a friendly gesture. Do try to adapt the code for your own purposes; imitation may be flattering but copying is just mindless copying.
+Can I link to your site from mine?
+
+    Certainly. And you’ll be in some fine company if you do. 
+What about a link exchange? Will you link to my site from yours?
+
+    If I like your site, I’ll probably link to it, and I do enjoy looking at new and groovy sites so please send ’em in. I don’t do link exchanges. I appreciate the link, but I don’t link to people just because they’ve linked to me; it doesn’t work that way.
+How does your site know what music you’re listening to?
+
+    I use the AudioScrobbler plug-in for iTunes.
+
+    Regular readers will notice a few favourites popping up: The Wedding Present, Spiritualized, White Stripes, Super Furry Animals, Pavement, Deus, Lambchop; with a few bits of Gilles Peterson, Curtis Mayfield, Mr Scruff and Chemical Brothers thrown in for good measure.
+
+    In an early attempt at database design and dynamic web pages I stuck my whole music catalogue online which you can peruse at will. It’s not sad, it’s for insurance purposes; honest.
+What do you think of Jakob Nielsen?
+
+    I got asked this in an interview once. I’m not sure how I answered, but I was offered the job and took it. I’ll answer it properly for you some time.
+
+
 </section>
 
 <?php
