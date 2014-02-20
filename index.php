06eb02ea2939dee29e17698db8ed39aae8456108
Richard Rutter <rich@clagAir-2.local>
2014-02-20 23:25:13 +0000

    final layout tweaks

--- index.php
+++ index.php
@@ -54,6 +54,8 @@ include($dr . "header.inc.php");
 
 </header>
 
+<div class="newness">
+
 <article class="newestpost">
 <?php getpost($blogpostids[0]); ?>
 <p class="date"><time datetime="<?php echo $post_isodate[$blogpostids[0]] ?>"><?php echo $post_postdate[$blogpostids[0]] ?></time></p>
@@ -71,96 +73,100 @@ echo stripslashes($homeflickr);
 ?>
 </article>
 
+</div>
+
 
 <section class="live">
-	<section class="cluster twitter">
-	<h2><span><a href="http://twitter.com/clagnut" rel="me"><img src="/i/icon-twitter.png" alt="Follow me on Twitter" title="@clagnut" class="icon" /></a> @clagnut</span></h2>
-		 
-		 
-	<?php
-	getTwitter();
-	if (isset($twitter)) {
-		echo stripslashes($twitter);
-	} else {
-		echo "<article><p>No tweets available. Probably a fail whale scenario.</p></article>";
-	}
-	?>
-
-</section>
-	<div class="ambient"> <!-- td -->
-		<section class="cluster lastfm">
-	<h2><span><a href="http://www.last.fm/user/clagnut" rel="me"><img src="/i/icon-lastfm.png" alt="Last.fm" title="Last.fm" class="icon" /></a> Listening</span></h2>		 
-	
-	<?php
-	getLastfm();
-	if (isset($lastfm)) {
-		echo stripslashes($lastfm);
-	}
-	?>
-	
-</section>	
-		<section class="cluster kennedy">
-	<h2><span><a href="http://kennedyapp.com"><img src="/i/icon-kennedy.png" alt="Kennedy App" title="Kennedy App" class="icon" /></a> A Moment</span></h2>		 
-		 	
-
-	<article>
-		<?php		
-		# Get latest moment from Kennedy
-		include($dr3 . "/kennedy/moments.php");
-		
-		$moments = getMoments($dr3);
-		if(is_array($moments) && count($moments)>0) {
-			$moment = $moments[0];
-			echo formatMoment($moment);
+		<section class="cluster twitter">
+		<h2><span><a href="http://twitter.com/clagnut" rel="me"><img src="/i/icon-twitter.png" alt="Follow me on Twitter" title="@clagnut" class="icon" /></a> @clagnut</span></h2>
+			 
+			 
+		<?php
+		getTwitter();
+		if (isset($twitter)) {
+			echo stripslashes($twitter);
 		} else {
-			echo "<p>No moments available.</p>";
+			echo "<article><p>No tweets available. Probably a fail whale scenario.</p></article>";
 		}
 		?>
-	</article>
 	
-</section>	
-		<section class="cluster thisismyjam">
-	<h2><span><a href="https://www.thisismyjam.com/clagnut"><img src="/i/icon-jam.png" alt="ThisIsMyJam" title="ThisIsMyJam" class="icon" rel="me" /></a> Current Jam</span></h2>		 
-		 
-	<article>
-	<script src="http://www.thisismyjam.com/includes/js/medallion.js"></script>
-	<script>Jam.Medallion.insert({"username":"clagnut","imageSize":"medium"});</script>
-	</article>	
-	
-</section>	
-	</div>
-	<div class="mylatest">
-		<section class="cluster latestposts">
-	<h2><span>Latest Posts</span></h2>
-	<ul class='articles'>
-	<?php
-	
-	foreach ($blogpostids AS $key => $blogpostid) {
-		if ($key > 0 && $key < count($blogpostids)) {
-			getpost($blogpostid);
-			echo "<li><article>\n";
-			echo "	<p class=\"date\">\n";
-			echo "		<time datetime=\"" . $post_isodate[$blogpostid] . " \">" . $post_postdate[$blogpostid] . " </time>\n";
-			echo "	</p>\n";
-			echo "	<h1><a href=\"/blog/" . $blogpostid . "/\" rel=\"bookmark\">" . $post_title[$blogpostid] . " </a></h1>\n";
-			echo "	<p class=\"categories\">" . $post_categories[$blogpostid] . " </p>\n";
-			echo "</article></li>\n";
+	</section>
+		<div class="ambient"> <!-- td -->
+			<section class="cluster lastfm">
+		<h2><span><a href="http://www.last.fm/user/clagnut" rel="me"><img src="/i/icon-lastfm.png" alt="Last.fm" title="Last.fm" class="icon" /></a> Listening</span></h2>		 
+		
+		<?php
+		getLastfm();
+		if (isset($lastfm)) {
+			echo stripslashes($lastfm);
 		}
-	}
-	?>
-	</ul>
-
-</section>
-		<section class="gallery cluster latestphotos">
-	<h2><span><a href="http://flickr.com/photos/clagnut" rel="me"><img src="/i/icon-flickr" alt="Flickr" title="Flickr" class="icon" /></a> Latest Photos</span></h2>
+		?>
+		
+	</section>	
+			<section class="cluster kennedy">
+		<h2><span><a href="http://kennedyapp.com"><img src="/i/icon-kennedy.png" alt="Kennedy App" title="Kennedy App" class="icon" /></a> A Moment</span></h2>		 
+			 	
 	
-	<?php
-	getlatestFlickr();
-	if (isset($latestflickr)) {
-		echo stripslashes($latestflickr);
-	}
-	?>
-</section>
+		<article>
+			<?php		
+			# Get latest moment from Kennedy
+			include($dr3 . "/kennedy/moments.php");
+			
+			$moments = getMoments($dr3);
+			if(is_array($moments) && count($moments)>0) {
+				$moment = $moments[0];
+				echo formatMoment($moment);
+			} else {
+				echo "<p>No moments available.</p>";
+			}
+			?>
+		</article>
+		
+	</section>	
+			<section class="cluster thisismyjam">
+		<h2><span><a href="https://www.thisismyjam.com/clagnut"><img src="/i/icon-jam.png" alt="ThisIsMyJam" title="ThisIsMyJam" class="icon" rel="me" /></a> Current Jam</span></h2>		 
+			 
+		<article>
+		<script src="http://www.thisismyjam.com/includes/js/medallion.js"></script>
+		<script>Jam.Medallion.insert({"username":"clagnut","imageSize":"medium"});</script>
+		</article>	
+		
+	</section>	
+		</div>
+		<div class="mylatest">
+			<section class="cluster latestposts">
+		<h2><span>Latest Posts</span></h2>
+		<ul class='articles'>
+		<?php
+		
+		foreach ($blogpostids AS $key => $blogpostid) {
+			if ($key > 0 && $key < count($blogpostids)) {
+				getpost($blogpostid);
+				if($key==3) { echo "</ul>\n<ul class='articles'>"; }
+				echo "<li><article>\n";
+				echo "	<p class=\"date\">\n";
+				echo "		<time datetime=\"" . $post_isodate[$blogpostid] . " \">" . $post_postdate[$blogpostid] . " </time>\n";
+				echo "	</p>\n";
+				echo "	<h1><a href=\"/blog/" . $blogpostid . "/\" rel=\"bookmark\">" . $post_title[$blogpostid] . " </a></h1>\n";
+				echo "	<p class=\"categories\">" . $post_categories[$blogpostid] . " </p>\n";
+				echo "</article></li>\n";
+			}
+		}
+		?>
+		</ul>
+	
+	</section>
+			<section class="gallery cluster latestphotos">
+		<h2><span><a href="http://flickr.com/photos/clagnut" rel="me"><img src="/i/icon-flickr" alt="Flickr" title="Flickr" class="icon" /></a> Latest Photos</span></h2>
+		
+		<?php
+		getlatestFlickr();
+		if (isset($latestflickr)) {
+			echo stripslashes($latestflickr);
+		}
+		?>
+	</section>
+		</div>
 	</div>
 </section>
 
